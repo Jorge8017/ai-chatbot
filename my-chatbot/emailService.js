4b27fa7894db0b254c6e4650a26a7f9a5d9683e4
Jorge8017 <102513493+Jorge8017@users.noreply.github.com>
2024-10-24 11:54:44 +0200

    Update emailService.js

--- my-chatbot/emailService.js
+++ my-chatbot/emailService.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // Update this to match your React app's URL
+  origin: 'http://localhost:5173', 
   credentials: true,
 }));
 
